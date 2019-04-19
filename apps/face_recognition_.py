7febd4696eb218795a48a8a55232f55ad739999d
gregoiredervaux <gregoire.dervaux@2019.icam.fr>
2019-04-19 13:49:10 -0400

    1er commit

---
+++ apps/face_recognition_.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+import scipy
+from models import face_track_server, face_describer_server, face_db, camera_server
+from configs import configs
+import os
+import sys
+
+'''
+The demo app utilize all servers in model folder with simple business scenario/logics:
+I have a camera product and I need to use it to find all visitors in my store who came here before.
+
+Main logics is in the process function, where you can further customize.
+'''
+
+
+class Demo(camera_server.CameraServer):
+
+    def __init__(self, name, *args, **kwargs):
+        super(Demo, self).__init__(*args, **kwargs)
+        self.name = name
+        if not os.path.isfile(configs.db_path + name):
+            os.mkdir(configs.db_path + name)
+        self.face_tracker = face_track_server.FaceTrackServer()
+        self.face_describer = face_describer_server.FDServer(
+            model_fp=configs.face_describer_model_fp,
+            input_tensor_names=configs.face_describer_input_tensor_names,
+            output_tensor_names=configs.face_describer_output_tensor_names,
+            device=configs.face_describer_device)
+        self.face_db = face_db.Model()
+        self.face_db.add_class()
+        self.trained = False
+
+    def processs(self, frame):
+
+        # Step1. Find and track face (frame ---> [Face_Tracker] ---> Faces Loactions)
+        self.face_tracker.process(frame)
+        _faces = self.face_tracker.get_faces()
+
+        # Uncomment below to visualize face
+        #_faces_loc = self.face_tracker.get_faces_loc()
+        #self._viz_faces(_faces_loc, frame)
+
+        # Step2. For each face, get the cropped face area, feeding it to face describer (insightface) to get 512-D Feature Embedding
+        _face_descriptions = []
+        _num_faces = len(_faces)
+        if _num_faces == 0:
+            return
+        for _face in _faces:
+            #cv2.imshow("similaire test", _face)
+            #cv2.waitKey(0)
+            _face_resize = cv2.resize(_face, configs.face_describer_tensor_shape)
+            _data_feed = [np.expand_dims(_face_resize.copy(), axis=0), configs.face_describer_drop_out_rate]
+            _face_description = self.face_describer.inference(_data_feed)[0][0]
+            _face_descriptions.append(_face_description)
+
+            # Step3. For each face, check whether there are similar faces and if not save it to db.
+            # Below naive and verbose implementation is to tutor you how this work
+            dir = os.listdir(configs.db_path + self.name)
+            if len(dir) > 150:
+                cv2.imwrite(configs.db_path + self.name + "/" + str(len(dir) + 1), _face)
+            else:
+                self.face_db.train_new_class(configs.db_path + self.name)
+
+        print('[Demo] -----------------------------------------------------------')
+
+    def _viz_faces(self, faces_loc, frame):
+        for _face_loc in faces_loc:
+            x1 = int(_face_loc[0] * self.face_tracker.cam_w)
+            y1 = int(_face_loc[1] * self.face_tracker.cam_h)
+            x2 = int(_face_loc[2] * self.face_tracker.cam_w)
+            y2 = int(_face_loc[3] * self.face_tracker.cam_h)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        cv2.imshow('faces1', frame)
+        cv2.waitKey(1)
+
+
+if __name__ == '__main__':
+
+    demo = Demo(sys.argv[1], camera_address=0)
+    demo.run()
+
---
+++ models/face_track_server.py
@@ -0,0 +1,72 @@
+import cv2
+import face_recognition
+from faced import FaceDetector
+from faced.utils import annotate_image
+
+'''
+This server:
+    Input: Camera frame
+    Output: Relative locations for each face, with [(tr_x, tr_y, bl_x, bl_y)]
+
+x1,y1 ------
+|          |
+|          |
+|          |
+--------x2,y2
+'''
+
+
+class FaceTrackServer(object):
+
+    faces = []
+    face_locations = []
+    face_relative_locations = []
+    cam_h = None
+    cam_w = None
+    camera_address = None
+
+    def __init__(self, down_scale_factor=0.25):
+        assert 0 <= down_scale_factor <= 1
+        self.down_scale_factor = down_scale_factor
+        self.face_detector = FaceDetector()
+
+    def get_cam_info(self):
+        return {'camera': {'width': self.cam_w, 'height': self.cam_h, 'address': self.camera_address}}
+
+    def reset(self):
+        self.face_relative_locations = []
+        self.face_locations = []
+        self.faces = []
+
+    def process(self, frame):
+        self.reset()
+        self.cam_h, self.cam_w, _ = frame.shape
+        # Resize frame of video to 1/4 size for faster face recognition processing
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_img = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2RGB)
+        self.face_locations = self.face_detector.predict(rgb_img)
+        # Display the results
+        if len(self.face_locations) > 1:
+            self.face_locations = []
+
+        for x, y, w, h, _ in self.face_locations:
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            x1 = x - int(w / 2)
+            y1 = y - int(h / 2)
+            x2 = x + int(w / 2)
+            y2 = y + int(h / 2)
+
+            _face_area = frame[y1:y2, x1:x2, :]
+
+            if _face_area.size != 0:
+                self.faces.append(_face_area)
+
+        print('[FaceTracker Server] Found {} faces!'.format(len(self.faces)))
+        return self.faces
+
+    def get_faces_loc(self):
+        return self.face_locations
+
+    def get_faces(self):
+        return self.faces
